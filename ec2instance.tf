07e4929c865345918a4b737afd2c73720ab66019
Devops-Raja <105640321+Devops-Raja@users.noreply.github.com>
2025-06-13 23:31:37 +0530

    tf

---
+++ ec2instance.tf
@@ -0,0 +1,23 @@
+# This Terraform configuration creates an EC2 instance in the AWS region ap-south-1
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "aws" {
+    region="ap-south-1"
+}
+
+resource "aws_instance" "ec2instance" {
+  ami           = "ami-0f918f7e67a3323f0" # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ec2freetier"
+  }
+}
